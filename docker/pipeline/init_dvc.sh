ee117bcb0dcc41b6f73ea4f6316d6b2ee5917561
Ubuntu <ubuntu@ip-172-31-11-9.eu-west-1.compute.internal>
2026-02-26 07:32:23 +0000

    feat: complete MLOps ecosystem setup with MinIO, DVC and MLflow

--- docker/pipeline/init_dvc.sh
+++ docker/pipeline/init_dvc.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-# On initialise DVC s'il ne l'est pas déjà
-dvc init --no-scm -f
 
-# On configure le "Remote" (le stockage distant sur MinIO)
-dvc remote add -d minio s3://dvc-storage
+# Attendre que minio-init ait fini de créer les buckets
+sleep 15
+
+# Configure le remote DVC pointant vers MinIO
+dvc remote add -d minio s3://dvc-storage --force
 dvc remote modify minio endpointurl http://minio:9000
 
-# On lance la pipeline (le fichier dvc.yaml qui contient tes étapes)
+# Lance la pipeline
 dvc repro
 
-# On garde le conteneur actif pour qu'on puisse voir les logs
-tail -f /dev/null
+# Push les artefacts vers MinIO (requis pour le test 3)
+dvc push
